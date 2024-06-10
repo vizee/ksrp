2f78d44102251c041f185b5dad2550fdaea9ba4a
vizee <vizee@live.cn>
2024-06-10 22:46:23 +0800

    feat: show message when port API returns 204

--- cmd/ksrp-agent/port.go
+++ cmd/ksrp-agent/port.go
@@ -22,8 +22,11 @@ func getPort(port string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	if resp.StatusCode > 200 {
+	if resp.StatusCode >= 400 {
 		return nil, fmt.Errorf("API Error: %d %s", resp.StatusCode, string(respData))
+	}
+	if resp.StatusCode == http.StatusNoContent {
+		return nil, nil
 	}
 	return strings.Split(string(respData), "\n"), nil
 }
@@ -37,6 +40,10 @@ func portCommand() *cobra.Command {
 			portInfo, err := getPort(args[0])
 			if err != nil {
 				fatal("get port:", err)
+			}
+			if len(portInfo) == 0 {
+				fmt.Println("port is not in use")
+				return
 			}
 			for _, s := range portInfo {
 				s = strings.TrimSpace(s)
